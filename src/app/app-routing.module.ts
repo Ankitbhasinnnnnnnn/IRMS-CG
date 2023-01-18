85e9380b98d2d22ff9e2bb5adfe28a3ec9872279
Shivam Rai <shivam.rai@cginfinity.com>
2023-01-18 18:46:03 +0530

    added email verification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmailVerificationComponent } from './Components/email-verification/email-verification.component';
 import { LoginComponent } from './Components/login/login.component';
 import { SignupComponent } from './Components/signup/signup.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: SignupComponent },
+  { path: 'verify', component: EmailVerificationComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
