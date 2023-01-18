771e7b6be0d344f5c3243755e884487b2e193849
Ankitbhasinnnnnnnn <105282765+Ankitbhasinnnnnnnn@users.noreply.github.com>
2023-01-18 11:22:57 +0530

    login page UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './components/signup/signup.component';
+import { SignupComponent } from './Components/signup/signup.component';
 
 
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [AppComponent, SignupComponent, LoginComponent],
