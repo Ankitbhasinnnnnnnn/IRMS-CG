ca0ad62c5b5220c48ae5039823afdddaa336cbd4
Ankitbhasinnnnnnnn <105282765+Ankitbhasinnnnnnnn@users.noreply.github.com>
2023-01-27 14:28:38 +0530

    Add user detail page

---
+++ src/app/components/userdetail/userdetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'userdetail',
+  templateUrl: './userdetail.component.html',
+  styleUrls: ['./userdetail.component.scss']
+})
+export class UserdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
