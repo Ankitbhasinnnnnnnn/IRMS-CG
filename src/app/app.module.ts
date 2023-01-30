d10f71f739c3687c656a809bbe9c6f91d0a8904e
Shivam Rai <shivam.rai@cginfinity.com>
2023-01-30 12:23:05 +0530

    calender integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
-import {HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentsModule } from './components/component.module';
-import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './components/login/login.component';
 import { EmailVerificationComponent } from './components/email-verification/email-verification.component';
-import {CalendarModule} from 'primeng/calendar'
-import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
-import {MenuItem} from 'primeng/api';                  //api
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CalendarModule } from 'primeng/calendar';
+import { AccordionModule } from 'primeng/accordion'; //accordion and accordion tab
+import { MenuItem } from 'primeng/api'; //api
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
+
 @NgModule({
-  declarations: [AppComponent, SignupComponent, LoginComponent, EmailVerificationComponent,AdminLayoutComponent,AuthLayoutComponent ],
-  imports: [BrowserModule,BrowserAnimationsModule, AppRoutingModule, NgbModule,ComponentsModule,HttpClientModule,AccordionModule, CalendarModule],
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    EmailVerificationComponent,
+    AdminLayoutComponent,
+    AuthLayoutComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgbModule,
+    MatNativeDateModule,
+    ComponentsModule,
+    HttpClientModule,
+    AccordionModule,
+    CalendarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-
-import { FooterComponent } from "./footer/footer.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { SidebarComponent } from "./sidebar/sidebar.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarModule } from 'primeng/calendar';
+import { NavbarComponent } from '../Components/navbar/navbar.component';
+import { SidebarComponent } from '../Components/sidebar/sidebar.component';
+import { FooterComponent } from '../Components/footer/footer.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, NgbModule],
+  imports: [CommonModule, RouterModule, NgbModule, CalendarModule],
   declarations: [FooterComponent, NavbarComponent, SidebarComponent],
-  exports: [FooterComponent, NavbarComponent, SidebarComponent]
+  exports: [FooterComponent, NavbarComponent, SidebarComponent, CalendarModule],
 })
 export class ComponentsModule {}
