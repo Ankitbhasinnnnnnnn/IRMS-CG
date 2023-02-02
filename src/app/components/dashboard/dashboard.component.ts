b28ecd60225896780e74577f6202ef981417b303
Ankitbhasinnnnnnnn <105282765+Ankitbhasinnnnnnnn@users.noreply.github.com>
2023-02-02 17:54:46 +0530

    add card in dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-dashboard",
+  styleUrls: ["dashboard.component.scss"],
   templateUrl: "dashboard.component.html"
 })
 export class DashboardComponent implements OnInit {
