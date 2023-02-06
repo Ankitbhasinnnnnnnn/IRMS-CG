2b3591d0b6473c7a4f4e0bb612720516be9a0509
Ankitbhasinnnnnnnn <105282765+Ankitbhasinnnnnnnn@users.noreply.github.com>
2023-02-06 11:40:22 +0530

    video-panel

--- src/app/components/video-panel/video-panel.component.ts
+++ src/app/components/video-panel/video-panel.component.ts
@@ -41,54 +41,54 @@ export class VideoPanelComponent {
     
 };
  /* 2. Initialize method for YT IFrame API */
- init() {
-  // Return if Player is already created
-  if (window['YT']) {
-    this.startVideo();
-    return;
-  }  
+//  init() {
+//   // Return if Player is already created
+//   if (window['YT']) {
+//     this.startVideo();
+//     return;
+//   }  
 
-  var tag = document.createElement('script');
-    tag.src = 'https://www.youtube.com/iframe_api';
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    if(firstScriptTag.parentNode!=null)
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//   var tag = document.createElement('script');
+//     tag.src = 'https://www.youtube.com/iframe_api';
+//     var firstScriptTag = document.getElementsByTagName('script')[0];
+//     if(firstScriptTag.parentNode!=null)
+//     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    /* 3. startVideo() will create an <iframe> (and YouTube player) after the API code downloads. */
-    (window as any).onYouTubeIframeAPIReady = () => this.startVideo()
-}
-toggleVideo() {
-  if (this.showVideo)
-    this.showVideo = false;
-  else {
-    this.showVideo = true;
-    setTimeout(() => {
-      this.init()
-    })
-  }
-}
-startVideo() {
-  this.reframed = false;
-  this.player = new window['YT'].Player('player', {
-    videoId: this.video,
-    playerVars: {
-      autoplay: 1,
-      modestbranding: 1,
-      controls: 1,
-      disablekb: 1,
-      rel: 0,
-      showinfo: 0,
-      fs: 0,
-      playsinline: 1
+//     /* 3. startVideo() will create an <iframe> (and YouTube player) after the API code downloads. */
+//     (window as any).onYouTubeIframeAPIReady = () => this.startVideo()
+// }
+// toggleVideo() {
+//   if (this.showVideo)
+//     this.showVideo = false;
+//   else {
+//     this.showVideo = true;
+//     setTimeout(() => {
+//       this.init()
+//     })
+//   }
+// }
+// startVideo() {
+//   this.reframed = false;
+//   this.player = new window['YT'].Player('player', {
+//     videoId: this.video,
+//     playerVars: {
+//       autoplay: 1,
+//       modestbranding: 1,
+//       controls: 1,
+//       disablekb: 1,
+//       rel: 0,
+//       showinfo: 0,
+//       fs: 0,
+//       playsinline: 1
 
-    },
-    events: {
-      'onStateChange': this.onPlayerStateChange.bind(this),
-      'onError': this.onPlayerError.bind(this),
-      'onReady': this.onPlayerReady.bind(this),
-    }
-  });
-}
+//     },
+//     events: {
+//       'onStateChange': this.onPlayerStateChange.bind(this),
+//       'onError': this.onPlayerError.bind(this),
+//       'onReady': this.onPlayerReady.bind(this),
+//     }
+//   });
+// }
  /* 4. It will be called when the Video Player is ready */
  onPlayerReady(event: any) {
 
@@ -96,54 +96,13 @@ startVideo() {
 
 }
   /* 5. API will call this function when Player State changes like PLAYING, PAUSED, ENDED */
-  onPlayerStateChange(event: any) {
-    console.log(event)
-    switch (event.data) {
-      case window['YT'].PlayerState.PLAYING:
-        if (this.cleanTime() == 0) {
-          console.log('started ' + this.cleanTime());
-        } else {
-          console.log('playing ' + this.cleanTime())
-        };
-        //should add the code here 
-        if(!this.timespent.length){
-          for(var i=0, l=parseInt(this.player.getDuration()); i<l; i++) this.timespent.push(false);
-      }
-    this.timer = setInterval(this.record,100);
-  
-        break;
-      case window['YT'].PlayerState.PAUSED:
-        if (this.player.getDuration() - this.player.getCurrentTime() != 0) {
-          console.log('paused' + ' @ ' + this.cleanTime());
-        };
-        break;
-      case window['YT'].PlayerState.ENDED:
-        console.log('ended ');
-        break;
-    };
-  };
+
   cleanTime() {
     return Math.round(this.player.getCurrentTime())
   };
 
-  record(){
-    // this.timespent[ parseInt(this.player.getCurrentTime()) ] = true;
-    this.timespent[ parseInt(!this.player.getCurrentTime ? 0.0 : this.player.getCurrentTime()) ] = true;
-    this.showPercentage();
-  }
-  
-   showPercentage(){
-      var percent = 0;
-      for(var i=0, l=this.timespent.length; i<l; i++){
-          if(this.timespent[i]) percent++;
-      }
-      percent = Math.round(percent / this.timespent.length * 100);
-      let display  = document.getElementById('display');
-        if(display != null)
-          display.innerHTML = percent + "%";
-      
-      
-  }
+ 
+ 
   onPlayerError(event:any) {
     switch (event.data) {
       case 2:
@@ -502,42 +461,42 @@ startVideo() {
   //     console.log('Player is Ready');
 
 
-onYouTubeIframeAPIReady() {console.log("Player is Ready")
+// onYouTubeIframeAPIReady() {console.log("Player is Ready")
 
-	this.player = new YT.Player( 'player', {
-		events: { 'onStateChange': this.onPlayerStateChange }
-	});
-}
+// 	this.player = new YT.Player( 'player', {
+// 		events: { 'onStateChange': this.onPlayerStateChange }
+// 	});
+// }
 
- onPlayerStateChange(event:any) {
-console.log(event);
-	if(event.data === 1) { // Started playing
-  console.log("Started Playing");
-        if(!this.timespent.length){
-            for(var i=0, l=parseInt(this.player.getDuration()); i<l; i++) this.timespent.push(false);
-        }
-	    this.timer = setInterval(this.record,100);
-    } else {
-		clearInterval(this.timer);
-	}
-}
+//  onPlayerStateChange(event:any) {
+// console.log(event);
+// 	if(event.data === 1) { // Started playing
+//   console.log("Started Playing");
+//         if(!this.timespent.length){
+//             for(var i=0, l=parseInt(this.player.getDuration()); i<l; i++) this.timespent.push(false);
+//         }
+// 	    this.timer = setInterval(this.record,100);
+//     } else {
+// 		clearInterval(this.timer);
+// 	}
+// }
 
- record(){
-	this.timespent[ parseInt(this.player.getCurrentTime()) ] = true;
-	this.showPercentage();
-}
+//  record(){
+// 	this.timespent[ parseInt(this.player.getCurrentTime()) ] = true;
+// 	this.showPercentage();
+// }
 
- showPercentage(){
-    var percent = 0;
-    for(var i=0, l=this.timespent.length; i<l; i++){
-        if(this.timespent[i]) percent++;
-    }
-    percent = Math.round(percent / this.timespent.length * 100);
-    let display  = document.getElementById('display');
-      if(display != null)
-        display.innerHTML = percent + "%";
+//  showPercentage(){
+//     var percent = 0;
+//     for(var i=0, l=this.timespent.length; i<l; i++){
+//         if(this.timespent[i]) percent++;
+//     }
+//     percent = Math.round(percent / this.timespent.length * 100);
+//     let display  = document.getElementById('display');
+//       if(display != null)
+//         display.innerHTML = percent + "%";
     
     
-}
+// }
 
 }
